c3edfb92ad60d531fc7c803ee90621ed05006bd1
Pekka Väänänen <pekka.vaananen@umbra3d.com>
2019-06-26 12:28:29 +0300

    Add minified build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "rollup-plugin-node-resolve";
 import execute from "rollup-plugin-execute";
 import { eslint } from "rollup-plugin-eslint";
+import { terser } from "rollup-plugin-terser";
 import fs from "fs";
 
 const globals = { three: "THREE" };
@@ -15,6 +16,19 @@ function copyfiles(files) {
     }
   }
 }
+
+// This can't be just an array because the plugins need to be instantiated for each config.
+const commonPlugins = () => [
+  eslint({
+    include: "src/*"
+  }),
+  resolve(),
+  execute("mkdir dist"),
+  copyfiles([
+    ["node_modules/umbrajs/dist/umbra.wasm", "dist/umbra.wasm"]
+  ])
+];
+
 
 const config = [
   {
@@ -34,15 +48,28 @@ const config = [
       }
     ],
     external: ["three"],
+    plugins: commonPlugins()
+  },
+  {
+    input: "src/threesupport.js",
+    output: [
+      {
+        file: `dist/umbrajs-three.min.js`,
+        format: "umd",
+        name: "UmbraRuntime",
+        globals
+      },
+      {
+        file: `dist/umbrajs-three.amd.min.js`,
+        format: "amd",
+        name: "UmbraRuntime",
+        globals
+      }
+    ],
+    external: ["three"],
     plugins: [
-      eslint({
-	    include: "src/*"
-	  }),
-      resolve(),
-	  execute("mkdir dist"),
-      copyfiles([
-        ["node_modules/umbrajs/dist/umbra.wasm", "dist/umbra.wasm"],
-      ])
+      ...commonPlugins(),
+      terser()
     ]
   }
 ];
