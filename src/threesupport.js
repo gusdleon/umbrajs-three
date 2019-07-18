822c3c7895fc038ecf27b03ed6a1d9cf03abad9f
Pekka Väänänen <pekka.vaananen@umbra3d.com>
2019-07-18 22:51:53 +0300

    Add getBounds() and getCenter()

--- src/threesupport.js
+++ src/threesupport.js
@@ -95,6 +95,19 @@ ModelObject.prototype.getInfo = function () {
   return info
 }
 
+ModelObject.prototype.getBounds = function () {
+  if (!this.umbra.scene.isConnected()) {
+    return undefined
+  }
+
+  const info = this.umbra.scene.getInfo()
+  const bounds = info.bounds
+  const min = bounds.min
+  const max = bounds.max
+  let box = new THREE.Box3(new THREE.Vector3(min[0], min[1], min[2]), new THREE.Vector3(max[0], max[1], max[2]))
+  return box
+}
+
 function findLights (scene) {
   const lights = []
   scene.traverseVisible(obj => {
@@ -104,6 +117,14 @@ function findLights (scene) {
   })
 
   return lights
+}
+
+ModelObject.prototype.getCenter = function () {
+  const bounds = this.getBounds()
+  bounds.applyMatrix4(this.matrixWorld)
+  let center = new THREE.Vector3()
+  bounds.getCenter(center)
+  return center
 }
 
 ModelObject.prototype.pruneOldViews = function (frame) {
