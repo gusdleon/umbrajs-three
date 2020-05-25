b9772763e2a59e575032947e5056e8da41ee25bb
Pekka Väänänen <pekka.vaananen@umbra3d.com>
2020-05-25 18:10:44 +0300

    Update builds

--- dist/ThreeWrapper.d.ts
+++ dist/ThreeWrapper.d.ts
@@ -7,6 +7,6 @@ export { Loader, LoadingManager } from 'three';
 export { WebGLRenderer, Material, MeshBasicMaterial, MeshPhysicalMaterial, TangentSpaceNormalMap, } from 'three';
 export { Matrix4, Vector3, Box3, Sphere } from 'three';
 export { CompressedTexture, DataTexture, BufferGeometry, TypedArray, Float32BufferAttribute, } from 'three';
-export { LinearFilter, LinearEncoding, sRGBEncoding, TextureDataType, } from 'three';
+export { LinearFilter, LinearEncoding, sRGBEncoding, TextureDataType } from 'three';
 export { RGBFormat, RGBAFormat, LuminanceAlphaFormat, RGBA_S3TC_DXT1_Format, RGBA_S3TC_DXT5_Format, RGB_ETC1_Format, RGBA_ASTC_4x4_Format, RGB_PVRTC_4BPPV1_Format, } from 'three';
 export { UnsignedByteType, UnsignedShort565Type, HalfFloatType } from 'three';
